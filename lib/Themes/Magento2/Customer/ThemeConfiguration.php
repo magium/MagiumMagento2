ee93fce69c7c4e7a139c0b4a49ed39552ec412ef
Kevin Schroeder <kschroeder@mirageworks.com>
2016-05-10 14:18:19 -0500

    Navigation to order

--- lib/Themes/Magento2/Customer/ThemeConfiguration.php
+++ lib/Themes/Magento2/Customer/ThemeConfiguration.php
@@ -14,4 +14,10 @@ class ThemeConfiguration extends AbstractThemeConfiguration
     public $accountNavigationXpath  = '//div[@id="block-collapsible-nav"]/descendant::a[concat(" ",normalize-space(.)," ")=" {{%s}} "]';
 
     public $accountSectionHeaderXpath = '//h1[@class="page-title"]/descendant::span[concat(" ",normalize-space(.)," ")=" {{%s}} "]';
+
+    public $orderPageName = 'My Orders';
+
+    public $viewOrderLinkXpath = '//td[@data-th="{{Order #}}" and .="%s"]/../td[contains(concat(" ",normalize-space(@class)," ")," actions ")]/descendant::a[contains(concat(" ",normalize-space(@class)," ")," view ")]';
+
+    public $orderPageTitleContainsText = 'Order #';
 }
